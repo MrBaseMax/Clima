7c4e6f093a5baa3a818e687a3035aacd1e4a7499
MrBaseMax <shmaser@icloud.com>
2021-07-18 01:40:32 +0300

    Initial Commit

---
+++ Clima/WeatherManager.swift
@@ -0,0 +1,77 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Макс on 07.02.2021.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+
+protocol WeatherManagerDelegate {
+    func didUpdateWeather(_ weatherManager: WeatherManager, _ weather: WeatherModel)
+    func didFailWithError(error: Error)
+}
+
+struct WeatherManager{    
+    let weatherURL = "top_secret"
+    
+    
+    var delegate: WeatherManagerDelegate?
+    
+    
+    func fetchWeather(cityName: String){
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(with: urlString)
+        
+    }
+    
+    
+    func fetchWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) {
+        let urlString = "\(weatherURL)&lat=\(latitude)&lon=\(longitude)"
+        performRequest(with: urlString)
+    }
+    
+    
+    func performRequest(with urlString: String){
+        if let url = URL(string: urlString){
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    delegate?.didFailWithError(error: error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    if let weather = parseJSON(safeData) {
+                        delegate?.didUpdateWeather(self, weather)
+                    }
+                }
+            }
+            
+            task.resume()
+        }
+    }
+    
+    func parseJSON(_ weatherData: Data) -> WeatherModel? {
+        let decoder = JSONDecoder()
+        
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            let id = decodedData.weather[0].id
+            let temp = decodedData.main.temp
+            let name = decodedData.name
+            
+            return WeatherModel(conditionID: id, cityName: name, temperature: temp)
+            
+        } catch {
+            delegate?.didFailWithError(error: error)
+            return nil
+        }
+    }
+    
+    
+    
+}
---
+++ Clima/WeatherModel.swift
@@ -0,0 +1,44 @@
+//
+//  WeatherModel.swift
+//  Clima
+//
+//  Created by Макс on 09.02.2021.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+
+struct WeatherModel {
+    let conditionID: Int
+    let cityName: String
+    let temperature: Double
+    
+    var temperatureString: String {
+        return String( format: "%.1f", temperature )
+    }
+    
+    var conditionName: String {
+        switch conditionID {
+            case 200...299:
+                return "cloud.bolt.rain"
+            case 300...399:
+                return "cloud.drizzle"
+            case 500...599:
+                return "cloud.rain"
+            case 600...699:
+                return "cloud.snow"
+            case 700...799:
+                return "cloud.fog"
+            case 800:
+                return "sun.max"
+            case 801...899:
+                return "cloud"
+                
+            default:
+                return "xmark.icloud"
+        }
+    }
+    
+    
+}
